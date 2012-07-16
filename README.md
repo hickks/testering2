3b4cb2ab736b494ab183c04dfd70897a9c2478e0
Bert Ehrmann <bert@fai2.com>
2012-07-16 16:03:24 -0400

    First changes
    Hello, nurse!

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-testering2
+This is a test #2	
 ==========
 
-This is a test #2
+This is a test project, useless to all but me. 
---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+
+
+
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+<title>I am the homepage of the site</title>
+</head>
+
+<body>
+<p>Homepage</p>
+<p><img src="Screen Shot 2012-07-12 at 11.11.07 AM.png" width="866" height="564" /></p>
+</body>
+</html>
